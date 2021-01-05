7b89860081da3e84179318be46c62eb8b6dc2d18
chouzee <anton.ryabchenko.92@gmail.com>
2021-01-05 10:53:22 +0200

    return string in lower and upper cases

---
+++ strings/13.py
@@ -0,0 +1,8 @@
+user_input = input("What is your name? ")
+
+def to_upper_lower(user_input):
+    print("My name is " + user_input.lower())
+    print("My name is " + user_input.upper())
+
+
+to_upper_lower(user_input)

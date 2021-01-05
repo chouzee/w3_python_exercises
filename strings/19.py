0a8281968f660fbe8e61bbbffe1b24a6781363af
chouzee <anton.ryabchenko.92@gmail.com>
2021-01-05 14:57:05 +0200

    split link by delimiter

--- strings/19.py
+++ strings/19.py
@@ -1,4 +1,12 @@
 def get_last_part(delim, arg):
+    """ a little bit of note for myself....
+    1. we do split by right side and cut 
+    sentence by 2 parts, and leave a part after
+    delimiter, then we are printing only that
+    part, before delimiter. So,
+    (delim, 1) - will print 2 parts inside a list,
+    and by [0] we are printing part before delimiter. :)
+    """
     return (arg.rsplit(delim, 1)[0])
 
 
